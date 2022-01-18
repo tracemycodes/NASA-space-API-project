13bf70d82310956abc30200cf1d660765165ed66
Agba Emmanuel <agbaayoh@gmail.com>
2022-01-18 03:27:17 -0700

    responsive NASA api page

---
+++ app.js
@@ -0,0 +1,44 @@
+const nasa = new Nasa();
+const ui = new Ui();
+
+document.addEventListener("DOMContentLoaded", getNasa);
+const main = document.getElementById("profile__data")
+
+function load() {
+  let loadTime;
+
+  loadTime = true;
+  if (loadTime) {
+    main.innerHTML = `<div class="lds-ellipsis">
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
+</div>`;
+  }
+}
+
+function getNasa(params) {
+  load();
+
+  nasa
+    .getNasa()
+    .then((result) => {
+      loadTime = false;
+      
+
+      if (!loadTime) {
+        ui.layout(result);
+
+        const UIbutton = document.querySelectorAll(".button");
+        UIbutton.forEach((button) => {
+          button.addEventListener("click", function (e) {
+            ui.animateButton(button);
+            e.preventDefault();
+          });
+        });
+      }
+    })
+}
+
+ui.navbar();
